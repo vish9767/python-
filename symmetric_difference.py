11de7124876b62ba307233fdf618a5cd5b5465a7
vish9767 <91862990+vish9767@users.noreply.github.com>
2024-07-19 15:38:29 +0530

    Create symmetric_difference.py

---
+++ symmetric_difference.py
@@ -0,0 +1,12 @@
+#symmetric difference operator (^) on two sets, set1 and set2. The symmetric difference of two sets is the set of elements that are in either of the sets, but not in their intersection.
+set1={1,4,6,8}
+set2={11,22,76,6}
+print(set1^set2)
+
+#The symmetric difference set1 ^ set2 will contain elements that are in either s1 or s2, but not in both.
+
+
+
+#find difference in set datatype.
+
+print("this is difference ",set1.difference(set2))
